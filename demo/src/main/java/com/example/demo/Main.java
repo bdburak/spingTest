343692acd96a45614a9f59581e50b87f37a0809f
Burak Dogucu <bdogucu@butel.com.tr>
2024-01-22 17:34:13 +0300

    test is working

--- demo/src/main/java/com/example/demo/Main.java
+++ demo/src/main/java/com/example/demo/Main.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("gg");
+		SpringApplication.run(Main.class, args);
 	}
 
 }
